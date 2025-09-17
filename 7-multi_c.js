925a44540ca2a1bb0056cd42c73a1415affe176d
Cyrus Maina <77388640+Cyr0707@users.noreply.github.com>
2025-09-17 10:30:09 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,10 @@
 const x = parseInt(process.argv[2]);
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
+  // The loop will only execute if 'x' is a positive integer.
+  // If 'x' is 0 or a negative number, the loop condition (i < x) will be false,
+  // and no output will be printed, which matches the expected behavior.
   let i = 0;
   while (i < x) {
     console.log("C is fun");
